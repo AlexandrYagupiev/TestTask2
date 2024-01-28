311a0a7c55b1a2ff5ad9eaf4f0b7423f02664d26
Alex <siaghupiev@mail.ru>
2024-01-28 18:09:39 +0500

    Добавьте файлы проекта.

---
+++ TestTask2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace TestTask2
+{
+    public static class Program
+    {
+        static void Main(string[] args)//Тест методов 
+        {
+            Parallel.For(0, 50, i =>
+            {
+                if (i % 2 == 0)
+                {
+                    AddToCount(2);
+                }
+                else
+                {
+                    Console.WriteLine(GetCount());
+                }
+            });
+            Console.WriteLine(GetCount());
+            Console.WriteLine("End");
+            Console.ReadKey();
+        }
+
+        private static int count;
+        private static ReaderWriterLockSlim locker = new ReaderWriterLockSlim();
+
+        public static void AddToCount(int value)
+        {
+            locker.EnterWriteLock();
+            count += value;
+            locker.ExitWriteLock();
+        }
+
+        public static int GetCount()
+        {
+            locker.EnterReadLock();
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                locker.ExitReadLock();
+            }
+        }
+    }
+}
